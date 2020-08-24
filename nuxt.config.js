29ed11531808a8e194da37861aef91ce8115e080
中村昌暉 <smith@nakamuramasaakiranoMacBook-Pro.local>
2020-08-25 02:05:23 +0900

    Setup vform

--- nuxt.config.js
+++ nuxt.config.js
@@ -41,8 +41,7 @@ export default {
   ** Plugins to load before mounting the App
   ** https://nuxtjs.org/guide/plugins
   */
-  plugins: [
-  ],
+  plugins: ['~plugins/vform'],
   /*
   ** Auto import components
   ** See https://nuxtjs.org/api/configuration-components
