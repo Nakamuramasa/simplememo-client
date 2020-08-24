fdd4409586f383d894ab69a5ac40f05de317552b
中村昌暉 <smith@nakamuramasaakiranoMacBook-Pro.local>
2020-08-24 19:14:52 +0900

    Prepared project  setup

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,6 +23,11 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ],
+    script: [
+      { src: "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.slim.min.js" },
+      { src: "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.bundle.min.js" },
+      { src: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/js/all.min.js" }
     ]
   },
   /*
--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,29 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        simplememo-client
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+    <div class="container">
+        <div class="card mt-3">
+            <div class="card-body d-flex flex-row">
+                <i class="fas fa-user-circle fa-3x mr-1"></i>
+                <div>
+                    <div class="font-weight-bold">
+                        ユーザー名
+                    </div>
+                    <div class="font-weight-lighter">
+                        2020/2/1 12:00
+                    </div>
+                </div>
+            </div>
+            <div class="card-body pt-0 pb-2">
+                <h3 class="h4 card-title">
+                    記事タイトル
+                </h3>
+                <div class="card-text">
+                    記事本文
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-export default {}
+export default {};
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
