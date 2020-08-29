d578ed93dee074327a7c764364769bc947c927ac
中村昌暉 <smith@nakamuramasaakiranoMacBook-Pro.local>
2020-08-30 02:03:46 +0900

    Created Article like methods

--- components/ArticleLike.vue
+++ components/ArticleLike.vue
@@ -2,13 +2,19 @@
     <div class="card-body pt-0 pb-2 pl-3">
         <div class="card-text">
             <div>
-                <button
-                    type="button"
-                    class="btn m-0 p-1 shadow-none"
-                >
-                    <i class="fas fa-heart mr-1" />
-                </button>
-                {{ article.likes_count }}
+                <a href="#" class="btn m-0 p-1 shadow-none" @click.prevent="likeIt">
+                    <template  v-if="liked">
+                        <span>
+                            <i class="fas fa-heart mr-1 red-text" />
+                        </span>
+                    </template>
+                    <template v-else>
+                        <span>
+                            <i class="fas fa-heart mr-1" />
+                        </span>
+                    </template>
+                </a>
+                {{ count }}
             </div>
         </div>
     </div>
@@ -16,6 +22,28 @@
 
 <script>
 export default {
-    props:['article']
+    props:['article'],
+    data() {
+        return {
+            liked: this.article.liked,
+            count: this.article.likes_count
+        }
+    },
+    methods: {
+        likeIt(){
+            if(this.$auth.loggedIn){
+                this.liked ? this.decr() : this.incr()
+                this.liked = !this.liked
+            }
+        },
+        incr(){
+            this.$axios.post(`/articles/${this.article.id}/like`)
+            .then(res => this.count++);
+        },
+        decr(){
+            this.$axios.post(`/articles/${this.article.id}/like`)
+            .then(res => this.count--);
+        }
+    }
 };
 </script>
--- pages/article/show.vue
+++ pages/article/show.vue
@@ -1,71 +1,8 @@
 <template>
     <div class="container">
-        <div class="card mt-3">
-            <div class="card-body d-flex flex-row">
-                <i class="fas fa-user-circle fa-3x mr-1"></i>
-                <div>
-                    <div class="font-weight-bold">
-                        {{ article.user.username }}
-                    </div>
-                    <div class="font-weight-lighter">
-                        {{ article.created_at_dates.created_at }}
-                    </div>
-                </div>
-
-                <div class="ml-auto card-text" v-if="$auth.loggedIn && $auth.user.id === article.user.id">
-                    <div class="dropdown">
-                        <a data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <button type="button" class="btn btn-link text-muted m-0 p-2">
-                                <i class="fas fa-ellipsis-v"></i>
-                            </button>
-                        </a>
-                        <div class="dropdown-menu dropdown-menu-right">
-                            <a class="dropdown-item" :href="`/articles/${article.id}/edit`">
-                                <i class="fas fa-pen mr-1"></i>記事を更新する
-                            </a>
-                            <div class="dropdown-divider"></div>
-                            <a @click.prevent="destroyArticle" class="dropdown-item text-danger">
-                                <i class="fas fa-trash-alt mr-1"></i>記事を削除する
-                            </a>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-            <div class="card-body pt-0 pb-2">
-                <h3 class="h4 card-title">
-                    {{ article.title }}
-                </h3>
-                <div class="card-text">
-                    {{ article.body }}
-                </div>
-            </div>
-            <div class="card-body pt-0 pb-2 pl-3">
-                <div class="card-text">
-                    <div>
-                        <button
-                            type="button"
-                            class="btn m-0 p-1 shadow-none"
-                        >
-                            <i class="fas fa-heart mr-1" />
-                        </button>
-                        {{ article.likes_count }}
-                    </div>
-                </div>
-            </div>
-
-            <div class="card-body pt-0 pb-4 pl-3">
-                <div class="card-text line-height">
-                    <a
-                        class="border p-1 mr-1 mt-1 text-muted"
-                        v-for="(tag, i) in article.tag_list.tags"
-                        :key="`tag-${i}`"
-                        :href="`/tags/${article.tag_list.normalized[i]}`"
-                    >{{ tag }}</a>
-                </div>
-            </div>
-
-        </div>
+        <base-article
+            :article="article"
+        ></base-article>
     </div>
 </template>
 
@@ -81,17 +18,6 @@ export default {
             }else{
                 error({statusCode: 500, message: "Internal server error"})
             }
-        }
-    },
-    methods: {
-        destroyArticle(){
-            this.$axios.delete(`/article/${this.article.id}`)
-            .then(res => {
-                setTimeout(() => {
-                    this.$router.push({ name: 'index' })
-                }, 2000);
-            })
-            .catch(e => console.log(e))
         }
     }
 };
